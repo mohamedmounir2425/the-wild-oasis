02ce04d45421a3f0ed3cd63c54b609b80bd8f37a
Mohamed Mounir <mohamed.mounir2425@gmail.com>
2025-02-05 09:05:53 +0200

    🆕 Building booking details

--- src/App.jsx
+++ src/App.jsx
@@ -12,6 +12,7 @@ import { GlobalStyles } from "./styles/GlobalStyles";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { ReactQueryDevtools } from "@tanstack/react-query-devtools";
 import { Toaster } from "react-hot-toast";
+import BookingDetail from "./features/bookings/BookingDetail";
 
 function App() {
   const queryClient = new QueryClient({
@@ -34,6 +35,7 @@ function App() {
               <Route index element={<Navigate replace to="dashboard" />} />
               <Route path="dashboard" element={<Dashboard />} />
               <Route path="bookings" element={<Bookings />} />
+              <Route path="booking/:bookingId" element={<BookingDetail />} />
               <Route path="cabins" element={<Cabins />} />
               <Route path="users" element={<Users />} />
               <Route path="settings" element={<Settings />} />
