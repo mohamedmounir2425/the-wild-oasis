b57ea5d68be8263ed73d59de7fa5cedb230b43c0
Mohamed Mounir <mohamed.mounir2425@gmail.com>
2025-02-10 17:13:00 +0200

    Checkout and fix bug in booking filter pagination page number

--- src/features/bookings/BookingDetail.jsx
+++ src/features/bookings/BookingDetail.jsx
@@ -12,6 +12,7 @@ import { useMoveBack } from "../../hooks/useMoveBack";
 import { useBooking } from "./useBooking";
 import Spinner from "../../ui/Spinner";
 import { useNavigate } from "react-router-dom";
+import useCheckout from "../check-in-out/useCheckout";
 
 const HeadingGroup = styled.div`
   display: flex;
@@ -21,6 +22,8 @@ const HeadingGroup = styled.div`
 
 function BookingDetail() {
   const { booking, isLoading } = useBooking();
+  const { checkout, isCheckingOut } = useCheckout();
+
   const navigate = useNavigate();
   const moveBack = useMoveBack();
 
@@ -49,6 +52,11 @@ function BookingDetail() {
         {status === "unconfirmed" && (
           <Button onClick={() => navigate(`/checkin/${booking.id}`)}>
             Check in
+          </Button>
+        )}
+        {status === "checked-in" && (
+          <Button disabled={isCheckingOut} onClick={() => checkout(id)}>
+            Check out
           </Button>
         )}
         <Button variation="secondary" onClick={moveBack}>
