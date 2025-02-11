1f885446be191a3a9d4c0afb85b85bd1033e2df6
Mohamed Mounir <mohamed.mounir2425@gmail.com>
2025-02-11 19:46:17 +0200

    Authorization protected route

--- src/features/authentication/useLogin.js
+++ src/features/authentication/useLogin.js
@@ -1,15 +1,18 @@
-import { useMutation } from "@tanstack/react-query";
+import { useMutation, useQueryClient } from "@tanstack/react-query";
 import { Login as LoginApi } from "../../services/apiAuth";
 import { useNavigate } from "react-router-dom";
 import toast from "react-hot-toast";
+import supabase from "../../services/supabase";
 
 export function useLogin() {
+  const queryClient = useQueryClient();
   const navigate = useNavigate();
   const { mutate: login, isPending: isLoginLoading } = useMutation({
     mutationFn: ({ email, password }) => LoginApi({ email, password }),
-    onSuccess: (data) => {
-      console.log(data);
+    onSuccess: (user) => {
+      console.log(user);
 
+      queryClient.setQueriesData(["user"], user.user);
       navigate("/");
     },
     onError: (err) => {
