62522360362175223d6b8bdd89e84d84d900b0a0
Mohamed Mounir <mohamed.mounir2425@gmail.com>
2025-02-13 23:38:27 +0200

    compute recent bookings & stays

---
+++ src/features/dashboard/useRecentStays.js
@@ -0,0 +1,22 @@
+import { useQuery } from "@tanstack/react-query";
+import { getStaysAfterDate } from "../../services/apiBookings";
+import { useSearchParams } from "react-router-dom";
+import { subDays } from "date-fns";
+
+function useRecentStays() {
+  const [searchParams] = useSearchParams();
+  const numDays = !searchParams.get("last")
+    ? 7
+    : Number(searchParams.get("last"));
+  const queryDate = subDays(new Date(), numDays).toISOString();
+  const { data: stays, isLoading2 } = useQuery({
+    queryFn: () => getStaysAfterDate(queryDate),
+    queryKey: ["stays", `last-${numDays}`],
+  });
+  const confirmedStays = stays?.filter(
+    (stay) => stay.status === "checked-in" || stay.status === "checked-out"
+  );
+  return { stays, isLoading2, confirmedStays, numDays };
+}
+
+export default useRecentStays;
