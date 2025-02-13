d66a1a46b61f7cd0a66b508d6507f6215ac1d291
Mohamed Mounir <mohamed.mounir2425@gmail.com>
2025-02-13 22:46:31 +0200

    🚀 create dark mode 🌙

---
+++ src/context/DarkModeContext.jsx
@@ -0,0 +1,32 @@
+import { createContext, useContext, useEffect } from "react";
+import { useLocalStorageState } from "../hooks/useLocalStorageState";
+
+const DarkModeContext = createContext();
+
+const DarkModeProvider = ({ children }) => {
+  const [isDarkMode, setIsDarkMode] = useLocalStorageState(false, "light-mode");
+  const toggleDarkMode = () => setIsDarkMode((d) => !d);
+  useEffect(() => {
+    if (isDarkMode) {
+      document.documentElement.classList.add("dark-mode");
+      document.documentElement.classList.remove("light-mode");
+    } else {
+      document.documentElement.classList.add("light-mode");
+      document.documentElement.classList.remove("dark-mode");
+    }
+  }, [isDarkMode]);
+  return (
+    <DarkModeContext.Provider value={{ isDarkMode, toggleDarkMode }}>
+      {children}
+    </DarkModeContext.Provider>
+  );
+};
+
+const useDarkMode = () => {
+  const context = useContext(DarkModeContext);
+  if (context === undefined)
+    throw new Error("DarkModeContext used outside the dark mode provider");
+  return context;
+};
+
+export { useDarkMode, DarkModeProvider };
--- src/ui/HeaderMenu.jsx
+++ src/ui/HeaderMenu.jsx
@@ -1,19 +1,26 @@
 import styled from "styled-components";
 import ButtonIcon from "./ButtonIcon";
-import { HiOutlineUser } from "react-icons/hi2";
+import { HiOutlineMoon, HiOutlineSun, HiOutlineUser } from "react-icons/hi2";
 import Logout from "./Logout";
 import { useNavigate } from "react-router-dom";
+import { useDarkMode } from "../context/DarkModeContext";
 const StyledHeaderMenue = styled.ul`
   display: flex;
   gap: 0.4rem;
 `;
 function HeaderMenu() {
   const navigate = useNavigate();
+  const { isDarkMode, toggleDarkMode } = useDarkMode();
   return (
     <StyledHeaderMenue>
       <li>
         <ButtonIcon onClick={() => navigate("/account")}>
           <HiOutlineUser />
+        </ButtonIcon>
+      </li>
+      <li>
+        <ButtonIcon onClick={toggleDarkMode}>
+          {!isDarkMode ? <HiOutlineMoon /> : <HiOutlineSun />}
         </ButtonIcon>
       </li>
       <li>
